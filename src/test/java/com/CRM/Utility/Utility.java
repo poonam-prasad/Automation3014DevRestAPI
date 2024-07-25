0b0e406c41c0c8c9d8bca3cfa25e1bd5cdf74b7e
poonam <poonamprasad94@gmail.com>
2024-07-25 14:31:22 +0530

    Utility package with ScreenShot class added

--- src/test/java/com/CRM/Utility/Utility.java
+++ src/test/java/com/CRM/Utility/Utility.java
@@ -17,6 +17,7 @@ public class Utility {
 		File dest=new File(System.getProperty("user.dir")+"//Screenshots//Test"+System.currentTimeMillis()+".png");
 		try {
 			FileHandler.copy(temp, dest);
+			System.out.println("test github commit");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
